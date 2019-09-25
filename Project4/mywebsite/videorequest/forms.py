62fff4949ca438faf5def9ca9f9439efad1e12cb
Orynbassarov Zhantore <zhanto97@gmail.com>
2019-09-24 22:28:24 -0700

    Project 4 (finished)

---
+++ Project4/mywebsite/videorequest/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+class VideoForm(forms.Form):
+	videoname = forms.CharField(max_length = 100, widget = forms.TextInput(attrs = {
+		'class': "form-control",
+		'placeholder': "Name",
+		'id': "inputName"
+	}))
+	videodesc = forms.CharField(widget = forms.Textarea({
+		'class': "form-control",
+		'rows': "5", 
+		'id': "comment",
+		'placeholder': "Comment"
+	}))
--- Project4/mywebsite/videorequest/views.py
+++ Project4/mywebsite/videorequest/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Video
+from .forms import VideoForm
 # Create your views here.
 
 def index(request):
-	return render(request, 'videorequest/index.html')
+	videos = Video.objects.order_by('-date')
+	context = {'videos': videos}
+	return render(request, 'videorequest/index.html', context)
 
 def vrform(request):
-	return render(request, 'videorequest/vrform.html')
+	if request.method == 'POST':
+		form = VideoForm(request.POST)
+		if form.is_valid():
+			new_req = Video(title = request.POST['videoname'], description = request.POST['videodesc'])
+			new_req.save()
+			return redirect('index')
+	else:
+		form = VideoForm()
+
+	context = {'form': form}
+	return render(request, 'videorequest/vrform.html', context)
